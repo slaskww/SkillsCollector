bb829234be5339dd165b911fb97571abca406b3c
lukasz <slaskww@gmail.com>
2019-08-04 12:50:14 +0200

    Add classes Skill, Source, User, add HibernateInitializer

---
+++ src/main/java/com/github/slaskww/skillscollector/listeners/HibernateInitializer.java
@@ -0,0 +1,55 @@
+package com.github.slaskww.skillscollector.listeners;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import java.util.Properties;
+
+@WebListener
+public class HibernateInitializer implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+
+        try {
+            Configuration configuration = new Configuration();
+
+            Properties hbnProperties = new Properties();
+            hbnProperties.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
+            hbnProperties.put(Environment.URL, "jdbc:mysql://localhost:3306/skills_collector?useSSL=false&serverTimezone=UTC");
+            // Nazwę użytkownika dostosuj do swojej instalacji MySQL
+            hbnProperties.put(Environment.USER, "root");
+            // Hasło użytkownika dostosuj do swojej instalacji MySQL
+            hbnProperties.put(Environment.PASS, "p2r2pet0wk2");
+            hbnProperties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+            hbnProperties.put(Environment.SHOW_SQL, "true");
+            hbnProperties.put(Environment.FORMAT_SQL, "true");
+            hbnProperties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+            // W przypadku gdy silnik Hibernate ma tworzyć schemat bazy danych, to poniżej
+            // użyj opcji create-drop albo update
+            hbnProperties.put(Environment.HBM2DDL_AUTO, "validate");
+            configuration.setProperties(hbnProperties);
+
+            // Odkomentuj poniższe instrukcje po utworzeniu klas encji (kolejne zadania)
+
+            //configuration.addAnnotatedClass(User.class);
+            //configuration.addAnnotatedClass(Source.class);
+            //configuration.addAnnotatedClass(Skill.class);
+
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+            SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            sce.getServletContext().setAttribute("session_factory", sessionFactory);
+
+        } catch (Exception e) {
+;        }
+
+
+    }
+}
---
+++ src/main/java/com/github/slaskww/skillscollector/model/User.java
@@ -0,0 +1,90 @@
+package com.github.slaskww.skillscollector.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "users")
+public class User  {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String username;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) &&
+                Objects.equals(firstName, user.firstName) &&
+                Objects.equals(lastName, user.lastName) &&
+                Objects.equals(password, user.password) &&
+                Objects.equals(username, user.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, password, username);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", password='" + password + '\'' +
+                ", username='" + username + '\'' +
+                '}';
+    }
+}
