fbee9d329ec4ac7d4022754f9b9121361ba6c648
lukasz <slaskww@gmail.com>
2019-08-04 14:59:56 +0200

    Add EncodingFilter

--- src/main/java/com/github/slaskww/skillscollector/filters/EncodingFilter.java
+++ src/main/java/com/github/slaskww/skillscollector/filters/EncodingFilter.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebFilter("/*")
+@WebFilter("/*") //wszystko, to co po znaku '/'
 public class EncodingFilter extends HttpFilter {
 
     @Override
